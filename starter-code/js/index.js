1f0c39c10077c8c87beaebb246359655e3c4d361
Llovet Nicholas <nllovet20199@columbushs.com>
2019-12-05 12:09:56 -0500

    needs fix

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -4,8 +4,9 @@ var $calc = document.getElementById('calc');
 function updateSubtot($product) {
   // Iteration 1.1
   let priceUnit = document.getElementsByClassName('pu');
-  let num = priceUnit[0].innerText;
-  num = priceUnit[0].innerText.replace('$', "")
+  let num = priceUnit[1].innerText;
+
+  num = priceUnit[1].innerText.replace('$', "")
   let price = parseFloat(num);
   console.log(price);
   
@@ -16,7 +17,7 @@ function updateSubtot($product) {
 
   let subtot = document.getElementsByClassName('subtot');
   subtot.innerText = "";
-  subtot[0].innerText = `$${numQ * price}`;
+  subtot[1].innerText = `$${numQ * price}`;
 
   return `${numQ * price}`;
 }
@@ -24,10 +25,23 @@ function updateSubtot($product) {
 function calcAll() {
   // Iteration 1.2
   let all = document.getElementsByClassName('product');
+  let ar = [];
+
+  let result = 0;
+  console.log(`ALL length is : ${all.length}`)
+ 
+  for (var i = 0; i < all.length; i++) {
+    let t = updateSubtot(all)
+    result = all[i];
+  }
+ 
   // console.log(all);
+
+console.log(result);
+
   let total = document.getElementById('total');
-  let result = updateSubtot(all);
-  total.innerText = result; 
+
+  // total.innerText = result; 
 }
 
 $calc.onclick = calcAll;
